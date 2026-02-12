0368514743bf87b914fe38115961fbd5896f3b7d
Shakir Mukhtar <shakir.mukhtar@oneguardian.in>
2026-02-12 19:49:24 +0530

    Hero section no video

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
     });
 
     navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
-        backgroundColor: '#00000050',
-        backdropFilter: 'blur(10px)',
+        backgroundColor: '#00000030',
+        backdropFilter: 'blur(5px)',
         duration: 1,
         ease: 'power1.inOut'
     })
