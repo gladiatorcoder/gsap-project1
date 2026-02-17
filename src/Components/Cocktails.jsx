a031beaa42c01ab99bbb97ac9ab43bf1f218cf59
Shakir Mukhtar <shakir.mukhtar@oneguardian.in>
2026-02-17 11:37:55 +0530

    Implemented cocktails section

--- src/Components/Cocktails.jsx
+++ src/Components/Cocktails.jsx
@@ -1,6 +1,27 @@
-import { cocktailLists, mockTailLists } from "../../constants"
+import { useGSAP } from "@gsap/react";
+import { cocktailLists, mockTailLists } from "../../constants";
+import gsap from 'gsap';
 
 const Cocktails = () => {
+
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#cocktails",
+            start: 'top 30%',
+            end: 'bottom 80%',
+            scrub: true
+        }
+    });
+
+    parallaxTimeline.from('#c-left-leaf', {
+        x: -100, 
+        y: 100
+    }).from('#c-right-leaf', {
+        x: 100,
+        y: 100
+    })
+  });
 
   return (
     <section id="cocktails">
